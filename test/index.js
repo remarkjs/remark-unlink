edb3f1dc0fa530d16fe2dadcbe3763a4b8b9336d
Titus Wormer <tituswormer@gmail.com>
2023-09-20 17:19:36 +0200

    Change to use `exports`

--- test/index.js
+++ test/index.js
@@ -3,11 +3,11 @@ import fs from 'node:fs/promises'
 import process from 'node:process'
 import test from 'node:test'
 import {remark} from 'remark'
-import remarkUnlink from '../index.js'
+import remarkUnlink from 'remark-unlink'
 
 test('remarkUnlink', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('remark-unlink')).sort(), [
       'default'
     ])
   })
